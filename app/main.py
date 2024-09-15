873224e8461d0d7e1670384605f2adb7be627b93
herminioneto <herminio_neto@outlook.com>
2024-09-15 12:50:58 -0300

    Adicionado CORS

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import task as task_router
 from app.routers import user as user_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router, prefix="/api", tags=["users"])
 app.include_router(task_router.router, prefix="/api", tags=["tasks"])
